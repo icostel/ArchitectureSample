b4c69a2670f4c4f528922c6c1a60b0ee46208140
icostel <ignat.costel@gmail.com>
2018-10-29 09:10:43 +0200

    some refactoring

--- app/src/main/java/com/icostel/arhitecturesample/ui/listusers/UserDiffCallback.java
+++ app/src/main/java/com/icostel/arhitecturesample/ui/listusers/UserDiffCallback.java
@@ -32,16 +32,12 @@ public class UserDiffCallback extends DiffUtil.Callback {
 
     @Override
     public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
-        return oldUserList.get(oldItemPosition).getFirstName().equalsIgnoreCase(newuserList.get(newItemPosition).getFirstName())
-                && oldUserList.get(oldItemPosition).getLastName().equalsIgnoreCase(newuserList.get(newItemPosition).getLastName())
-                && oldUserList.get(oldItemPosition).getCountry().equalsIgnoreCase(newuserList.get(newItemPosition).getCountry())
-                && oldUserList.get(oldItemPosition).getAge() == newuserList.get(newItemPosition).getAge();
+        return oldUserList.get(oldItemPosition).getId().equals(newuserList.get(newItemPosition).getId());
     }
 
     @Override
     public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
-        return oldUserList.get(oldItemPosition).getId()
-                .equalsIgnoreCase(newuserList.get(newItemPosition).getId());
+        return oldUserList.get(oldItemPosition).equals(newuserList.get(newItemPosition));
     }
 
     @Nullable

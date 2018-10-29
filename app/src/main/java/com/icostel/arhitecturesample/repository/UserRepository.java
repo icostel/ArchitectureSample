964e8a150b79b1e3d7c4163e93f991bba003a581
icostel <ignat.costel@gmail.com>
2018-10-29 12:51:39 +0200

    revert to working version

--- app/src/main/java/com/icostel/arhitecturesample/repository/UserRepository.java
+++ app/src/main/java/com/icostel/arhitecturesample/repository/UserRepository.java
@@ -35,7 +35,16 @@ public class UserRepository {
     public Observable<Optional<User>> getUserById(String userId) {
         // if we have the user in the DB, return it from there
         // if not do the API call, insert the user in DB and return it
+        // TODO concat with db call
+
         Timber.d("%s getUserById() %d", TAG, userId);
+        return this.usersApi.getUser(sessionStore.getUserSessionToken(), userId)
+                .subscribeOn(Schedulers.io())
+                .map(restUser -> {
+                    restUser.ifPresent(userDao::insert);
+                    return restUser;
+                });
+        /*
         return Observable.just(userDao.getUserById(userId))
                 .subscribeOn(Schedulers.io())
                 .flatMap(dbUser -> {
@@ -50,12 +59,24 @@ public class UserRepository {
                                 });
                     }
                 });
+                */
     }
 
     public Observable<Optional<List<User>>> getAllUsers() {
         // if we have users in the dao, return from there
         // if not do the API call, insert users in DB and return them
+        // TODO concat with db call
+
         Timber.d("%s getAllUsers()", TAG);
+
+        return this.usersApi.getUsers(sessionStore.getUserSessionToken())
+                .subscribeOn(Schedulers.io())
+                .map(users -> {
+                    // insert or update all users on call
+                    users.ifPresent(userDao::upsert);
+                    return users;
+                });
+        /*
         return Observable.just(userDao.getUsers())
                 .subscribeOn(Schedulers.io())
                 .flatMap(daoUsers -> {
@@ -71,6 +92,7 @@ public class UserRepository {
                                 });
                     }
                 });
+                */
     }
 
 }

87a949286453fb24033712e26766fca71faad335
icostel <ignat.costel@gmail.com>
2018-11-01 08:57:25 +0200

    updates to new navigation api

--- app/src/main/java/com/icostel/arhitecturesample/navigation/Navigator.java
+++ app/src/main/java/com/icostel/arhitecturesample/navigation/Navigator.java
@@ -2,6 +2,7 @@ package com.icostel.arhitecturesample.navigation;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.annotation.IdRes;
 import androidx.annotation.Nullable;
@@ -10,17 +11,13 @@ import androidx.fragment.app.FragmentManager;
 
 public interface Navigator {
     void navigateTo(@Nullable NavigationAction navigationAction);
-
-    @IdRes
-    int getFragmentContainer();
+    @IdRes int getFragmentContainer();
 
     FragmentManager getSupportFragmentManager();
-
     void startActivity(Intent intent);
-
+    void startActivity(Intent intent, Bundle options);
     void startActivityForResult(Intent intent, int requestCode);
-
     void finish();
-
     Context getContext();
+    void setResult(int resultCode, Intent data);
 }

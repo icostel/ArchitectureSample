9fc9cb9febace7cf0dc3412e3a57be806ee743da
icostel <ignat.costel@gmail.com>
2018-11-14 15:46:52 +0200

    converted app executors to kt

---
+++ app/src/main/java/com/icostel/arhitecturesample/utils/AppExecutors.kt
@@ -0,0 +1,57 @@
+package com.icostel.arhitecturesample.utils
+
+import android.os.Handler
+import android.os.Looper
+
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AppExecutors @Inject
+internal constructor() {
+
+    private val diskIO: Executor
+    private val networkIO: Executor
+    private val mainThread: Executor
+    private val background: Executor
+
+    init {
+        diskIO = Executors.newSingleThreadExecutor()
+        networkIO = Executors.newFixedThreadPool(NETWORK_THREADS)
+        mainThread = MainThreadExecutor()
+        background = Executors.newSingleThreadExecutor()
+    }
+
+    fun diskIO(): Executor {
+        return diskIO
+    }
+
+    fun networkIO(): Executor {
+        return networkIO
+    }
+
+    fun mainThread(): Executor {
+        return mainThread
+    }
+
+    fun background(): Executor {
+        return background
+    }
+
+    private class MainThreadExecutor : Executor {
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+
+        override fun execute(runnable: Runnable) {
+            mainThreadHandler.post(runnable)
+        }
+    }
+
+    companion object {
+
+        private val NETWORK_THREADS = 3
+    }
+}
+
